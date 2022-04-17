bd601717f499301250e618bb7fef4d09450b9487
Md. Shahriar Parvez <shahriar@sharetrip.net>
2022-04-17 11:37:32 +0600

    merge to main

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,21 +1,12 @@
-const stringToSlug = string => {
+const stringToSlug = (string) => {
   return string
-    .trim()
     .toLowerCase()
-    // .replace(/[^\w ]+/g, '')
-    .replace(/ +/g, '-')
-    .replaceAll(".", '');
-
-
-    // .toLowerCase()
-    // .trim()
-    // .replace(/[^\w\s-]/g, '')
-    // .replace(/[\s_-]+/g, '-')
-    // .replace(/^-+|-+$/g, '');
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
 };
 
-
-
 module.exports = {
-  stringToSlug
-}
+  stringToSlug,
+};
